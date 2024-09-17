522ed65b81b6f11a745eb05f455ccad62baf9296
ejcaie <evancaie705@gmail.com>
2024-09-17 14:09:32 -0400

    Implemented PlayerMovement Method

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,10 +8,33 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public float speed = 10f;
+    Vector3 velocity = new Vector3(0, 0, 0);
 
-    void Update()
+    void FixedUpdate()
     {
-
+        PlayerMovement();
     }
 
+    void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.y = speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity.y = -speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x = speed;
+        }
+        transform.position = transform.position + velocity * Time.deltaTime;
+        velocity = Vector3.zero;
+    }
 }
