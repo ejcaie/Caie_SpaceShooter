09b27f8fffc0d6c6ae6e683748674999cac3130c
ejcaie <evancaie705@gmail.com>
2024-09-23 13:21:58 -0400

    Task 1 Completed
    Task 1 A, B, and C completed.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,7 +8,9 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
-    public float speed = 10f;
+    public float speed = 0.1f;
+    public float maxSpeed = 10f;
+    public float acceleration = 0.0001f;
     Vector3 velocity = new Vector3(0, 0, 0);
 
     void FixedUpdate()
@@ -20,21 +22,29 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
+            while (speed < maxSpeed) speed = speed + acceleration;
             velocity.y = speed;
         }
         if (Input.GetKey(KeyCode.A))
         {
+            while (speed < maxSpeed) speed = speed + acceleration;
             velocity.x = -speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
+            while (speed < maxSpeed) speed = speed + acceleration;
             velocity.y = -speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
+            while (speed < maxSpeed) speed = speed + acceleration;
             velocity.x = speed;
         }
         transform.position = transform.position + velocity * Time.deltaTime;
-        velocity = Vector3.zero;
+        if (Input.anyKeyDown == false)
+        {
+            while (speed > 0.1) speed = speed - acceleration;
+            if (speed <= 0.1) velocity = Vector3.zero;
+        }
     }
 }
