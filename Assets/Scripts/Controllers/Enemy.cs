8a151b11721359753c482bd5d83a4fc3d7a9c3fe
ejcaie <evancaie705@gmail.com>
2024-09-23 15:47:11 -0400

    Task 2 Complete
    Task 2 A and B completed

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor.Experimental.GraphView;
 
 public class Enemy : MonoBehaviour
 {
+    public Vector3 direction = new Vector3();
+    public float speed = 1f;
+    public float acceleration = 0.1f;
+
+    private void Start()
+    {
+        direction.x = Random.Range(-100, 100);
+        direction.y = Random.Range(-100, 100);
+    }
     private void Update()
     {
+        EnemyMovement();
     }
 
+    public void EnemyMovement()
+    {
+        speed = speed + acceleration;
+        transform.position = transform.position + direction.normalized * speed * Time.deltaTime;
+    }
 }
