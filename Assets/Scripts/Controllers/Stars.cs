727f3922d4886de622e143d9aff11ba692ba3956
ejcaie <evancaie705@gmail.com>
2024-09-23 21:12:59 -0400

    Task 4 Completed
    Task 4 has been completed.

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -6,9 +6,18 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    public int i = -1;
 
     // Update is called once per frame
     void Update()
     {
+        DrawConsellation();
+        i++;
+        if (i >= starTransforms.Count - 1)  i = 0;
+    }
+    
+    public void DrawConsellation()
+    {
+        Debug.DrawLine(starTransforms[i].position, starTransforms[i + 1].position, Color.white, drawingTime);
     }
 }
